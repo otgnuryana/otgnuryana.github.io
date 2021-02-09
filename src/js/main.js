e6e03c6377e83bd8e9daa90831811afb74cb2cbf
Nuryana <otgnuryana@gmail.com>
2021-02-09 09:52:18 +0700

    Add animate on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
+// Navbar Toggle
 document.getElementById("hamburger").onclick = function toggleMenu() {
     const navToggle = document.getElementsByClassName("toggle");
     for (let i = 0; i < navToggle.length; i++) {
         navToggle.item(i).classList.toggle("hidden");
     }
 };
+
+// Animate on Scroll
+AOS.init();
